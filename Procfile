f5964dd80f9da829f2f7ae991683233f56716783
tgh15 <muhteguhadhiputra15@gmail.com>
2022-06-27 11:37:28 +0800

    add procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/apoteker.id_backend
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Unable to load env. Error: %s", err)
+	}
+
+	// get envs
+	port := os.Getenv("PORT")
+
+	// create new fiber app
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"hello": "world",
+		})
+	})
+
+	// listen to port
+	app.Listen(":" + port)
+}
