5c294940b5bc4a22d549bf696cedd5d9b836ed8f
tgh15 <muhteguhadhiputra15@gmail.com>
2022-06-28 14:20:31 +0800

    feat: add logger and json

--- go.mod
+++ go.mod
@@ -5,13 +5,17 @@ go 1.17
 require (
 	github.com/gofiber/fiber/v2 v2.34.1
 	github.com/joho/godotenv v1.4.0
+	go.uber.org/zap v1.21.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.37.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"log"
+	"time"
+
 	"apoteker.id_backend/config"
 	"apoteker.id_backend/router"
+	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
 )
 
 func main() {
+	// create logger
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("Unable to create logger %s", err)
+	}
+
+	// fiber config
+	fiberConf := &fiber.Config{
+		AppName:      "apoteker_backend v1.0.0",
+		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 60 * time.Second,
+		JSONEncoder:  json.Marshal,
+		JSONDecoder:  json.Unmarshal,
+	}
+
 	// create new fiber app
-	app := fiber.New()
+	app := fiber.New(*fiberConf)
 
 	router.SetupRoutes(app)
 
